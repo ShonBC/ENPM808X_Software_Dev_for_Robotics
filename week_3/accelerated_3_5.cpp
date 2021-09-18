c71ed4b1e74414eacd953d13e182a2d1bf1a06c4
ShonBC <scortes3@umd.edu>
2021-09-17 17:32:17 -0700

    init

--- week_3/accelerated_3_5.cpp
+++ week_3/accelerated_3_5.cpp
@@ -4,3 +4,115 @@
 //     and the second the final grades that can be computed as input is read. 
 // For now, you should assume a fixed number of homework grades. 
 // We'll see in §4.1.3/56 how to handle a variable number of grades intermixed with student names.
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+//Enter name
+//Store name in first vector
+//Enter X homework grades for each student 
+//Store grade in second vector
+//Compute Final grade
+
+std::string GetName()
+{
+
+    std::cout<<"Please enter Students name: " << std::endl;
+
+    std::string students;
+    std::cin >> students;
+
+    return students;
+
+}
+
+std::vector<double> GetGrades()
+{
+    
+    std::cout<<"Please enter 5 homework grades. Enter ';' when finished: " << std::endl;
+
+    std::vector<double> grades;
+
+    double x;
+
+    while (std::cin >> x)
+        {
+            grades.push_back(x);
+        }
+
+    return grades;
+
+}
+
+double FinalGrade(std::vector<double> grade_list)
+{
+    double total = 0;
+
+    for(double grade : grade_list)
+    {
+        total += grade;
+    }
+
+    int size = grade_list.size();
+
+    double final_grade = total / size;
+
+    return final_grade;
+}
+
+int main()
+{
+
+    std::vector<std::string> students_list;
+    std::vector<double> grade_list;
+    bool more_students = true;
+
+    while(more_students)
+    {
+
+        //Store Students Name in vector.  
+        std::string name = GetName();
+        students_list.push_back(name);
+
+        //Store homework grades in a vector.
+        std::vector<double> homework_grades;
+        homework_grades = GetGrades();
+
+        //Compute final grade and store it in the grade list.
+        double final_grade = FinalGrade(homework_grades);
+        std::cout<<final_grade<<std::endl;
+        grade_list.push_back(final_grade);
+
+        
+
+        std::cout<<"Would you like to enter another students grades? (Yes or No)"<<std::endl;
+        std::string decision = "";
+        std::cin>> decision;
+
+        if (decision != "Yes")
+        {
+            break;            
+        }
+
+    }
+
+    for(std::string name : students_list)
+    {
+
+        std::cout<<name;
+
+    }
+
+    std::cout<<std::endl;
+
+    for(double grade : grade_list)
+    {
+
+        std::cout<<grade;
+
+    }
+    std::cout<<std::endl;
+    
+
+}
