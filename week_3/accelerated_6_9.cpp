2ac5a7027ac3267ae3729c974540693e5e9583f0
ShonBC <scortes3@umd.edu>
2021-09-18 17:42:11 -0700

    init

--- week_3/accelerated_6_9.cpp
+++ week_3/accelerated_6_9.cpp
@@ -1,1 +1,23 @@
 // Use a library algorithm to concatenate all the elements of a vector<string>.
+
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+
+std::string Concatenate(std::vector<std::string> vec)
+{
+
+    std::string new_str = std::accumulate(vec.begin(), vec.end(), std::string(""));
+
+    return new_str;
+
+}
+
+int main()
+{
+
+    std::vector<std::string> test = 
+    Concatenate(test)
+
+}
