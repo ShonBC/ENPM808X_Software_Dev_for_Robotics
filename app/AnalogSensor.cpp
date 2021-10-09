2feca5c7ed5f68b74c2e0195544feca1563de077
Shon Cortes <scortes3@umd.edu>
2021-10-09 15:10:40 -0700

    Fixed linting and memory leak errors.

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -3,19 +3,18 @@
 #include <vector>
 
 AnalogSensor::AnalogSensor(unsigned int samples)
-    : mSamples(samples)
-{
+    : mSamples(samples) {
 }
 
-AnalogSensor::~AnalogSensor()
-{
+AnalogSensor::~AnalogSensor() {
 }
 
-int AnalogSensor::Read()
-{
+int AnalogSensor::Read() {
     std::vector<int> *readings = new std::vector<int>(mSamples, 10);
 
-    double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
+    double result = std::accumulate(readings->begin(), readings->end(), 0.0)
+    / readings->size();
+    delete readings;
     return result;
 }
 
